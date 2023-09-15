667b1e073b2783f57c42874bf610618b0651f64e
Gustavo Navarro <genr1818@gmail.com>
2023-09-15 17:26:18 -0600

    Server Side Rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import fetch from 'isomorphic-fetch'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+const apiUrl = 'https://avocado-app-wine.vercel.app/api/avo'
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+export const getServerSideProps = async () => {
+  const response = await fetch(apiUrl)
+  const { data: productList }: TAPIAvoResponse = await response.json()
 
+  return {
+    props: {
+      productList
+    }
+  }
+}
+
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
