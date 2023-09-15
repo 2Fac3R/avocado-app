70520de2e64d262891baac8895992ebe6d780896
Gustavo Navarro <genr1818@gmail.com>
2023-09-15 12:43:26 -0600

    Destructuring query params

--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -2,9 +2,12 @@ import React from 'react'
 import { useRouter } from 'next/router'
 
 const ProductItem = () => {
-  const router = useRouter()
+  const {
+    query: { productId }
+  } = useRouter()
+
   return (
-    <div>ProductItem: {router.query.id}</div>
+    <div>ProductItem: {productId}</div>
   )
 }
 
