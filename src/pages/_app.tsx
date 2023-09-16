311630a5291001ef614024ddfc7df4ae28575203
Gustavo Navarro <genr1818@gmail.com>
2023-09-15 18:26:37 -0600

    ReportWebVitals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app'
+import type { NextWebVitalsMetric } from 'next/app'
 import 'semantic-ui-css/semantic.min.css'
 import '../global.css'
 
 import CartProvider from 'store/Cart'
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  // https://web.dev/vitals/
+  // https://nextjs.org/docs/pages/api-reference/functions/use-report-web-vitals
+  console.log(metric)
+}
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Aditional props
